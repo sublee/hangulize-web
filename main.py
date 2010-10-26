db26add59e27e93810e50bf33582b2ce2940ad89
Heungsub Lee <heung@sublee.kr>
2010-10-27 01:55:45 +0900

    jQuery Mobile 사용

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ for dirname in os.listdir(libpath):
     sys.path.insert(0, os.path.join(libpath, dirname))
 from google.appengine.ext.webapp.util import run_wsgi_app
 from flask import *
-from hangulize import hangulize, langs
+from hangulize import hangulize
 
 
 app = Flask(__name__)
-locales = langs.__all__
 
+
+def get_locales():
+    import hangulize.langs
+    for loc in hangulize.langs.__all__:
+        __import__('hangulize.langs.%s' % loc)
+        yield (loc, getattr(getattr(hangulize.langs, loc), loc))
 
 @app.route('/favicon.ico')
 def favicon():
@@ -20,7 +25,7 @@ def favicon():
 def index():
     word = request.args.get('word', '')
     locale = request.args.get('locale', 'it')
-    context = dict(word=word, locale=locale, locales=locales)
+    context = dict(word=word, locale=locale, locales=get_locales())
     if word:
         context['hangulized'] = hangulize(unicode(word), locale=locale)
         return render_template('result.html', **context)
