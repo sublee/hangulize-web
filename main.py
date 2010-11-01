c03ec872fd5785cf7ac58c2c6a7efbbcaf7c17e1
Heungsub Lee <heung@sublee.kr>
2010-11-01 18:42:46 +0900

    국제화

--- main.py
+++ main.py
@@ -3,26 +3,46 @@ import os
 libpath = os.path.abspath('lib')
 for dirname in os.listdir(libpath):
     sys.path.insert(0, os.path.join(libpath, dirname))
+sys.path.insert(0, libpath)
 from google.appengine.ext.webapp.util import run_wsgi_app
 from flask import *
+from flaskext.babel import Babel, gettext
 from hangulize import hangulize
 
 
+LOCALES = ['ko', 'en']
+
+
 app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'ko'
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the HTTP accepted language."""
+    return request.accept_languages.best_match(LOCALES)
 
 
 def get_langs():
+    """Returns the allowed languages in :mod:`hangulize`."""
     import hangulize.langs
     for loc in hangulize.langs.__all__:
         __import__('hangulize.langs.%s' % loc)
-        yield (loc, getattr(getattr(hangulize.langs, loc), loc))
+        lang = getattr(getattr(hangulize.langs, loc), loc)
+        lang_name = gettext(lang.__name__)
+        yield (loc, lang_name)
+
 
 @app.route('/favicon.ico')
 def favicon():
+    """Sends the favicon file."""
     return app.send_static_file('favicon.ico')
+
 
 @app.route('/')
 def index():
+    """The index page."""
     word = request.args.get('word', '')
     locale = request.args.get('locale', 'it')
     context = dict(word=word, locale=locale, langs=get_langs())
@@ -39,4 +59,3 @@ def index():
 
 if __name__ == '__main__':
     run_wsgi_app(app)
-
